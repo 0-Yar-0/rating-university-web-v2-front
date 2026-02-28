6bce8137d172563e84415f0916bacfd1bf46e181
0-Yar-0 <romashovabdrey111@gmail.com>
2026-02-28 22:36:05 +0300

    '1'

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    plugins: [react()],
-    server: { port: 5173 }
+  plugins: [react()],
+  preview: {
+    allowedHosts: [
+      'rating-university-web-v2-1.onrender.com', // домен фронтенда
+      'rating-university-2-0.onrender.com'       // домен бэкенда
+    ],
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
+  }
 })
